12371b9ef6967711f0f1681a19ffd81fb687b295
DEVTYPE <Informatik.on.GitHub@gmail.com>
2016-02-01 17:42:15 +0600

    ToDo List - Step 1 http://devtype.blogspot.com/2016/02/Sozdanie-proekta-iOS-Single-View-Application.html

---
+++ ToDo List/ToDo List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo List
+//
+//  http://devtype.blogspot.com/2016/02/Razrabotka-mobilnogo-prilozheniya-dlya-iPhone.html
+//  Copyright © 2016 DEVTYPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
