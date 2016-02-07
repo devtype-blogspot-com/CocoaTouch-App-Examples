a959c1d582fafabde2a97d859081460e11452c49
DEVTYPE <Informatik.on.GitHub@gmail.com>
2016-02-07 16:16:27 +0600

    [ToDo List] Программирование кнопки в iOS-приложении Пост: http://devtype.blogspot.com/2016/02/Programmirovanie-knopki-v-iOS-prilozhenii.html

--- ToDo List/ToDo List/DetailViewController.m
+++ ToDo List/ToDo List/DetailViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSave;
 
 @end
 
@@ -22,11 +23,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.buttonSave addTarget:self
+                        action:@selector(save)
+              forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)save {
+    NSLog(@"save");
 }
 
 @end
