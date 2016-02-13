1f3ef38bae49a7650309bc3a4e3743920246dd7b
DEVTYPE <Informatik.on.GitHub@gmail.com>
2016-02-13 16:38:41 +0600

    [ToDo List] Как пользоваться UITableView http://devtype.blogspot.com/2016/02/Kak-polzovatsya-UITableView.html

---
+++ ToDo List/ToDo List/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  ToDo List
+//
+//  http://devtype.blogspot.com/2016/02/Kak-polzovatsya-UITableView.html
+//  Copyright © 2016 DEVTYPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
