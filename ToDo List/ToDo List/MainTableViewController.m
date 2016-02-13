b736c6e8d6447fc434eb03d483adc1566e1e5932
DEVTYPE <Informatik.on.GitHub@gmail.com>
2016-02-13 18:19:35 +0600

    [ToDo List] Стандартная анимация компонентов в iPhone-приложении http://devtype.blogspot.com/2016/02/Standartnaya-animatsiya-komponentov-v-iPhone-prilozhenii.html

--- ToDo List/ToDo List/MainTableViewController.m
+++ ToDo List/ToDo List/MainTableViewController.m
@@ -32,6 +32,7 @@
     NSArray *array = [[UIApplication sharedApplication] scheduledLocalNotifications];
     self.arrayEvents = [[NSMutableArray alloc] initWithArray:array];
 
+    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationFade];
 }
 
 - (void)didReceiveMemoryWarning {
